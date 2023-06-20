fc6d2cbe7a210f89cf4a2f3bb8fe5bdf0f7b87c6
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2023-06-20 16:40:56 -0300

    add tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,11 @@
-import { clsx } from 'clsx'
+import { clsx } from "clsx"
 
-import { AppHooks } from './app-hooks'
+import { AppHooks } from "./app-hooks"
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body
-        style={{ opacity: 0 }}
-        className={clsx(
-          basementGrotesque.variable,
-          neueMontreal.variable,
-          basementGrotesqueDisplay.variable,
-          basementGrotesqueExpanded.variable
-        )}
-      >
+      <body style={{ opacity: 0 }} className={clsx()}>
         <AppHooks />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 const HomePage = () => {
-  return (
-    <>
-    </>
-  )
+  return <></>
 }
 
 export default HomePage
